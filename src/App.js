688a13861936b1a9f3c0fce25ba350097e1ca8c5
Vesper <veswill3@gmail.com>
2017-06-15 17:35:57 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,171 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      board: [
+        [0,0,0,3,0,0,0,0,0],
+        [1,0,0,5,0,0,0,2,0],
+        [0,0,6,0,0,0,7,9,0],
+        [4,0,0,0,0,0,0,7,0],
+        [0,2,7,0,0,0,8,0,0],
+        [5,0,3,0,8,0,0,4,9],
+        [0,0,0,0,7,0,0,0,0],
+        [8,0,0,0,6,0,1,0,0],
+        [0,0,4,0,0,1,0,3,0]
+      ],
+      selected: 0,
+    };
+    this.keydownHandler = this.keydownHandler.bind(this);
+  }
+
+  componentDidMount() {
+    document.body.addEventListener('keydown', this.keydownHandler);
+  }
+
+  componentWillUnmount() {
+    document.body.removeEventListener('keydown', this.keydownHandler);
+  }
+
+  keydownHandler(e) {
+    // right arrow or space
+    if (e.keyCode === 39 || e.keyCode === 32) {
+      this.updateHighlight(++this.state.selected);
+    } else if (e.keyCode === 37) { // left arrow
+      this.updateHighlight(--this.state.selected);
+    }
+    e.preventDefault();
+  }
+
+  handleClick(r, c) {
+    let newBoard = this.state.board.map(function(row) {
+      return row.slice();
+    });
+    newBoard[r][c] = this.state.selected;
+    this.setState({board: newBoard});
+  }
+
+  renderCell(heightlightObj, r, c) {
+    let classes = [];
+    if (heightlightObj && heightlightObj[r + ':' + c]) {
+      classes.push("highlight");
+    }
+    if (this.state.selected === this.state.board[r][c]) {
+      classes.push("selected");
+    }
+    return (
+      <td className={classes.join(" ")}
+          onClick={() => this.handleClick(r, c)}>
+        {this.state.board[r][c] ? this.state.board[r][c] : " "}
+      </td>
+    );
+  }
+
+  renderRow(heightlightObj, r) {
+    return (
+      <tr>
+        {this.renderCell(heightlightObj, r, 0)}
+        {this.renderCell(heightlightObj, r, 1)}
+        {this.renderCell(heightlightObj, r, 2)}
+        {this.renderCell(heightlightObj, r, 3)}
+        {this.renderCell(heightlightObj, r, 4)}
+        {this.renderCell(heightlightObj, r, 5)}
+        {this.renderCell(heightlightObj, r, 6)}
+        {this.renderCell(heightlightObj, r, 7)}
+        {this.renderCell(heightlightObj, r, 8)}
+      </tr>
+    );
+  }
+
+  updateHighlight(val) {
+    if (val > 9) {
+      val = 0;
+    }
+    this.setState({selected: val});
+  }
+
+  render() {
+    let toHighlight = {};
+    if (this.state.selected) {
+      for (let r = 0; r < this.state.board.length; r++) {
+        for (let c = 0; c < this.state.board[r].length; c++) {
+          if (this.state.board[r][c]) {
+            toHighlight[r + ':' + c] = true;
+          }
+          if (this.state.board[r][c] === this.state.selected) {
+            // highlight row
+            for (let col = 0; col < 9; col++) {
+              toHighlight[r + ':' + col] = true;
+            }
+            // highlight column
+            for (let row = 0; row < 9; row++) {
+              toHighlight[row + ':' + c] = true;
+            }
+            // highlight group
+            let gR = 0;
+            if (r > 5) {
+              gR = 6;
+            } else if (r > 2) {
+              gR = 3;
+            }
+            let gC = 0;
+            if (c > 5) {
+              gC = 6;
+            } else if (c > 2) {
+              gC = 3;
+            }
+            for (var xR = gR + 2; xR >= gR; xR--) {
+              for (var xC = gC + 2; xC >= gC; xC--) {
+                toHighlight[xR + ':' + xC] = true;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return (
+      <div className="App">
+        <h2>Sudoku visual helper</h2>
+        <p>
+          Visually show remaining possibilities while solving a sudoku puzzle.
+          This is not a solver, it just helps to quickly review the board.
+        </p>
+        <table className="board">
+          <tbody>
+            {this.renderRow(toHighlight, 0)}
+            {this.renderRow(toHighlight, 1)}
+            {this.renderRow(toHighlight, 2)}
+            {this.renderRow(toHighlight, 3)}
+            {this.renderRow(toHighlight, 4)}
+            {this.renderRow(toHighlight, 5)}
+            {this.renderRow(toHighlight, 6)}
+            {this.renderRow(toHighlight, 7)}
+            {this.renderRow(toHighlight, 8)}
+          </tbody>
+        </table>
+        <div>
+          <button onClick={() => this.updateHighlight(0)}>C</button>
+          <button onClick={() => this.updateHighlight(1)}>1</button>
+          <button onClick={() => this.updateHighlight(2)}>2</button>
+          <button onClick={() => this.updateHighlight(3)}>3</button>
+          <button onClick={() => this.updateHighlight(4)}>4</button>
+          <button onClick={() => this.updateHighlight(5)}>5</button>
+          <button onClick={() => this.updateHighlight(6)}>6</button>
+          <button onClick={() => this.updateHighlight(7)}>7</button>
+          <button onClick={() => this.updateHighlight(8)}>8</button>
+          <button onClick={() => this.updateHighlight(9)}>9</button>
+          <button onClick={() => this.updateHighlight(++this.state.selected)}>+</button>
+        </div>
+        <p>
+          Click a number below to show it's availible slots. Click any slot to insert that number.
+          Cylce through numbers with the [+] button, spacebar, or left/right arrows.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
